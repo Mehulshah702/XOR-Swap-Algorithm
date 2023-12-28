7b289f64623116fea388ecc62f1627a9636c99c8
Mehul Shah <138091393+Mehulshah702@users.noreply.github.com>
2023-12-28 09:21:49 +0530

    Added C Language code.
    Using two variables, namely, a & b.

---
+++ xor.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void swapWithoutThirdVariable(int *a, int *b) {
+
+    *a = *a ^ *b;
+    *b = *a ^ *b;
+    *a = *a ^ *b;
+}
+
+int main() {
+    int x, y;
+
+    printf("Enter the first number: ");
+    scanf("%d", &x);
+
+    printf("Enter the second number: ");
+    scanf("%d", &y);
+
+    printf("Before swapping: x = %d, y = %d\n", x, y);
+
+    swapWithoutThirdVariable(&x, &y);
+
+    printf("After swapping: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
